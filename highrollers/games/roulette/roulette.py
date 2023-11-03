b988bf9ff49b020d247c556b2df2ceaf595e3f1c
Luna Steed <lunasteed@isu.edu>
2023-11-02 18:38:19 -0600

    Added method to generate a random number, and a method to check bets against most categories.

--- highrollers/games/roulette/roulette.py
+++ highrollers/games/roulette/roulette.py
@@ -1,5 +1,4 @@
 import random_api
-
 """
 Luna Steed
 High Rollers Roulette Class
@@ -8,29 +7,33 @@ Contains the logic for the game of Roulette.
 
 class Roulette:
     def __init__(self):
-        rednums = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 36]
-        blacknums = [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35]
+        """Initializing Function"""
+        self.numsDict = {"red": [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 36],
+                         "black": [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35],
+                         "green": [0, 37],
+                         "row 1": [1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34],
+                         "row 2": [2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35],
+                         "row 3": [3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36],
+                         "twelves 1": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+                         "twelves 2": [13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24],
+                         "twelves 3": [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36],
+                         "first half": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18],
+                         "second half": [19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36],
+                         "evens": [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36],
+                         "odds": [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35]
+                         }
 
-        """37 will represent 00"""
-        greennums = [0, 37]
+    def spin(self) -> int:
+        """Generates a random number from 0 - 37, with 37 acting as 00."""
+        rand = random_api.RandomAPI
+        self.spunnum = rand.random_integer(self, 0, 37, 1)[0]
+        return self.spunnum
 
-        firstrow = [1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34]
-        secondrow = [2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35]
-        thirdrow = [3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36]
-
-    def spin(self,
-             numbers: list[int] = None,
-             colors: int = 0,
-             evens: bool = False,
-             odds: bool = False,
-             row1: int = 0,
-             row2: int = 0,
-             row3: int = 0,
-             first12: int = 0,
-             second12: int = 0,
-             third12: int = 0,
-             firsthalf: bool = False,
-             secondhalf: bool = False):
-            rand = random_api.RandomAPI
-
-
+    def check_bet(self, category: str) -> bool:
+        """Checks the spun number against the dictionary given the category of bet made"""
+        if self.spunnum is None:
+            raise ValueError("Roulette wheel has not been spun yet!")
+        if self.spunnum in self.numsDict.get(category):
+            return True
+        else:
+            return False
